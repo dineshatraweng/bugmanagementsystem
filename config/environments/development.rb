bcde21763d2205755770007568d99d9a1866ea86
dineshatraweng <dinesh.bangar@raweng.com>
2013-12-10 20:31:59 +0530

    commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,19 @@ Bugmanagementsystems::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+ config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "raweng.com",
+    :authentication => :login,
+    :user_name => "dinesh.bangar@raweng.com",
+    :password => "dinesh12345"
+  }
+
+
+
+
 end
